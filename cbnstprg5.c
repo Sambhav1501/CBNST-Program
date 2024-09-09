fc5688eb0dbfc90ce6ba4f2f3242a15621cea774
Sambhav1501 <116714713+Sambhav1501@users.noreply.github.com>
2024-09-09 14:22:12 +0530

    Create cbnstprg5.c
    regular falsi

---
+++ cbnstprg5.c
@@ -0,0 +1,45 @@
+//regular falsi
+#include<stdio.h>
+#include<math.h>
+
+float f(int x)
+{
+	return (x*x)*(exp(-x/2))-1;
+}
+
+int main()
+{
+	float x0,x1,x2,f0,f1,f2,error;
+	int max_steps=0;
+	int step=1;
+	printf("Enter two approximations x0,x1");
+	scanf("%f%f",&x0,&x1);
+	printf("Enter allowed error");
+	scanf("%f",&error);
+	printf("Enter allowed max steps");
+	scanf("%d",&max_steps);
+	f0=f(x0);
+	f1=f(x1);
+	if(f0*f1>0)
+    {
+		printf("f(x0)*f(x1)>0, please choose another pair of approximations\n");
+    }
+    do
+    {
+    	if(f(x0)*f(x1)>0)
+        {
+            printf("fail condition met");
+            break;
+        }
+        x2=x2-((x1-x0)/(f1-f0))*f0;
+        printf("x = %f\n",x2);
+        step++;
+        f2=f(x2);
+        f0=f1;
+		f1=f2;
+		x0=x1;
+		x1=x2;
+    }while(fabs(f2)>error);
+    printf("root is %f",x2);
+	return 0;
+}
