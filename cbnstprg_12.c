ecc6d550b33648586a9bb51149dde7408285c758
Sambhav Jain <116714713+Sambhav1501@users.noreply.github.com>
2024-12-06 22:33:19 +0530

    Simpson's 3/8 Rule

---
+++ cbnstprg_12.c
@@ -0,0 +1,43 @@
+//Simpson's 3/8 Rule
+#include <stdio.h>
+#include <math.h>
+
+double func(double x)
+{
+    return (1/(1+x*x));
+}
+
+int main()
+{
+    int n, a, b;
+    printf("\nEnter number of intervals: ");
+    scanf("%d", &n);
+    printf("Enter lower limit(a): ");
+    scanf("%d", &a);
+    printf("Enter upper limit(b): ");
+    scanf("%d", &b);
+    double h = (b - a) / (float)n;
+    double x[n + 1];
+    double y[n + 1];
+    int count = 0;
+    printf("\nx\t\ty\n");
+    for (int i = 0; i <= n; i++)
+    {
+        x[i] = a + count * h;
+        count++;
+        y[i] = func(x[i]);
+        printf("%f\t%f\n", x[i], y[i]);
+    }
+    //applying formula
+    double sum_o = 0;
+    double sum_e = 0;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        if (i % 3 == 0)
+            sum_e += y[i];
+        else
+            sum_o += y[i];
+    }
+    double ans = (3 * h / 8) * (y[0] + y[n] + 3 * sum_o + 2 * sum_e);
+    printf("\nApproximate value of integration is: %f \n", ans);
+}
